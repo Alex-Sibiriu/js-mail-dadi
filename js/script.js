e6519175cd989cd2c9acf4bd9f4947ef0db2e417
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-02-23 20:59:15 +0100

    [fix] Javascript Optimization

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // MAIL
-const validMail = ['ugodeughi@email.it', 'alexsibiriu@email.it', 'boolean@email.it','ciao@ciao.it'];
+const validMail = ['ugodeughi@email.it', 'alex@email.it', 'boolean@email.it', 'ciao@ciao.it', 'a@a.a'];
 const sendBtn = document.querySelector('.js-send');
 const accessResult = document.querySelector('.js-access-result')
 
@@ -9,22 +9,23 @@ sendBtn.addEventListener('click', function(event) {
   event.preventDefault();
 
   const userMail = document.querySelector('.js-user-email').value;
+  let resultColor;
 
   for (let i = 0; i < validMail.length; i++) {
     if (userMail === validMail[i]) {
       accessResult.innerHTML = 'Accesso Consentito';
-      accessResult.classList.add('text-success');
-      accessResult.classList.remove('text-danger');
+      resultColor = 'text-success';
       playBtn.classList.remove('disabled');
-      i = validMail.length;
-      //Ma return non era meglio?
+      i = validMail.length;  //Volevo usare return ma non sapevo se era utilizzabile in questo esercizio
     } else {
       accessResult.innerHTML = 'Accesso Negato';
-      accessResult.classList.add('text-danger');
-      accessResult.classList.remove('text-success');
+      resultColor = 'text-danger';
       playBtn.classList.add('disabled');
     }
   }
+
+  accessResult.classList.remove('text-danger', 'text-success');
+  accessResult.classList.add(resultColor);
 })
 
 // DICE
@@ -33,24 +34,23 @@ const pcDice = document.querySelector('.js-computer-dice');
 const diceResult = document.querySelector('.js-dice-result');
 
 playBtn.addEventListener('click', function() {
-
+  
+  let resultColor;
+  
   userDice.innerHTML = Math.ceil(Math.random() * 6);
   pcDice.innerHTML = Math.ceil(Math.random() * 6);
 
   if (userDice.innerHTML > pcDice.innerHTML) {
     diceResult.innerHTML = 'Hai Vinto!';
-    diceResult.classList.add('text-success');
-    diceResult.classList.remove('text-danger');
-    diceResult.classList.remove('text-warning');
+    resultColor = 'text-success';
   } else if (userDice.innerHTML < pcDice.innerHTML){
     diceResult.innerHTML = 'Hai Perso!';
-    diceResult.classList.add('text-danger');
-    diceResult.classList.remove('text-success');
-    diceResult.classList.remove('text-warning');
+    resultColor = 'text-danger';
   } else {
     diceResult.innerHTML = 'Pareggio!';
-    diceResult.classList.add('text-warning');
-    diceResult.classList.remove('text-success');
-    diceResult.classList.remove('text-danger');
+    resultColor = 'text-warning';
   }
+
+  diceResult.classList.remove('text-success', 'text-danger', 'text-warning');
+  diceResult.classList.add(resultColor);
 })
