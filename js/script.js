b3713028a259de7ff0c6760f7e7eff2ee501db18
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-02-23 17:00:07 +0100

    [add] You can't play with wrong mails

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const validMail = ['ugodeughi@email.it', 'alexsibiriu@email.it', 'boolean@email.
 const sendBtn = document.querySelector('.js-send');
 const accessResult = document.querySelector('.js-access-result')
 
+const playBtn = document.querySelector('.js-play');
+
 sendBtn.addEventListener('click', function(event) {
   event.preventDefault();
 
@@ -13,41 +15,42 @@ sendBtn.addEventListener('click', function(event) {
       accessResult.innerHTML = 'Accesso Consentito';
       accessResult.classList.add('text-success');
       accessResult.classList.remove('text-danger');
+      playBtn.classList.remove('disabled');
       i = validMail.length;
       //Ma return non era meglio?
     } else {
       accessResult.innerHTML = 'Accesso Negato';
       accessResult.classList.add('text-danger');
       accessResult.classList.remove('text-success');
+      playBtn.classList.add('disabled');
     }
   }
 })
 
 // DICE
-const playBtn = document.querySelector('.js-play');
 const userDice = document.querySelector('.js-user-dice');
 const pcDice = document.querySelector('.js-computer-dice');
 const diceResult = document.querySelector('.js-dice-result');
 
 playBtn.addEventListener('click', function() {
 
-userDice.innerHTML = Math.ceil(Math.random() * 6);
-pcDice.innerHTML = Math.ceil(Math.random() * 6);
+  userDice.innerHTML = Math.ceil(Math.random() * 6);
+  pcDice.innerHTML = Math.ceil(Math.random() * 6);
 
-if (userDice.innerHTML > pcDice.innerHTML) {
-  diceResult.innerHTML = 'Hai Vinto!';
-  diceResult.classList.add('text-success');
-  diceResult.classList.remove('text-danger');
-  diceResult.classList.remove('text-warning');
-} else if (userDice.innerHTML < pcDice.innerHTML){
-  diceResult.innerHTML = 'Hai Perso!';
-  diceResult.classList.add('text-danger');
-  diceResult.classList.remove('text-success');
-  diceResult.classList.remove('text-warning');
-} else {
-  diceResult.innerHTML = 'Pareggio!';
-  diceResult.classList.add('text-warning');
-  diceResult.classList.remove('text-success');
-  diceResult.classList.remove('text-danger');
-}
+  if (userDice.innerHTML > pcDice.innerHTML) {
+    diceResult.innerHTML = 'Hai Vinto!';
+    diceResult.classList.add('text-success');
+    diceResult.classList.remove('text-danger');
+    diceResult.classList.remove('text-warning');
+  } else if (userDice.innerHTML < pcDice.innerHTML){
+    diceResult.innerHTML = 'Hai Perso!';
+    diceResult.classList.add('text-danger');
+    diceResult.classList.remove('text-success');
+    diceResult.classList.remove('text-warning');
+  } else {
+    diceResult.innerHTML = 'Pareggio!';
+    diceResult.classList.add('text-warning');
+    diceResult.classList.remove('text-success');
+    diceResult.classList.remove('text-danger');
+  }
 })
